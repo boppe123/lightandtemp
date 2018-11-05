6025dab7af8309a5ba3ebbb988a564133067a0b7
boppe123 <44640763+boppe123@users.noreply.github.com>
2018-11-05 12:52:05 +0100

    Add files via upload

--- indextest.js
+++ indextest.js
@@ -51,7 +51,7 @@ function callThingApi () {
     //let path = '/update?api_key=116UAXMQP1O8EYZ3&field1=1';
     // Make the HTTP request
 	
-    https.get('https://api.thingspeak.com/channels/617048/feeds.json?results=1', (res) => {
+    https.get('https://api.thingspeak.com/channels/592740/feeds.json?results=2', (res) => {
       let body = ''; // var to store the response chunks
       res.on('data', (d) => { body += d; }); // store each response chunk
       res.on('end', () => {
